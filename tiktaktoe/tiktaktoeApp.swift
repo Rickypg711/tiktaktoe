cb5eea973adc30048f869d92c1782f31d9498538
Rickypg711 <77697094+Rickypg711@users.noreply.github.com>
2023-07-10 16:12:33 -0700

    Initial Commit

---
+++ tiktaktoe/tiktaktoeApp.swift
@@ -0,0 +1,17 @@
+//
+//  tiktaktoeApp.swift
+//  tiktaktoe
+//
+//  Created by Ricardo Paredes on 7/10/23.
+//
+
+import SwiftUI
+
+@main
+struct tiktaktoeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
